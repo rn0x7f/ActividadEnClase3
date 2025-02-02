b9dd098a4ea327e1b7f301fc4000462219f6b15c
rn0x7f <ampheriz0@gmail.com>
2025-02-02 17:51:44 -0600

    .

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,10 @@
+
+// math_tests.cpp
+// This file contains unit tests for MyMathLib using Google Test.
+
 #include <gtest/gtest.h>
 #include "MyMathLib.h"
 
-TEST(MathTestCase, MathTestSqrt9)
-{
-	ASSERT_EQ(3, mySqrt(9));
+TEST(math_test_case, math_test_sqrt9) {
+    ASSERT_EQ(3, mySqrt(9));
 }
