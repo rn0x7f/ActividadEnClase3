3c49ca3a2f1471adf35034bcd87c8df3d6b67564
rn0x7f <ampheriz0@gmail.com>
2025-02-02 17:42:21 -0600

    solve issue 1

--- main.cpp
+++ main.cpp
@@ -7,10 +7,10 @@ int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "Sqrt(9) = " << mySqrt(9) << '\n';
-    std::cout << "Suma(5+5) = " << Suma(5, 5) << '\n';
-    std::cout << "Resta(5-5) = " << Resta(5, 5) << '\n';
-    std::cout << "Multiplicacion(5*5) = " << Multiplicacion(5, 5) << '\n';
-    std::cout << "Division(5/5) = " << Division(5, 5) << '\n';
+    std::cout << "Suma(5+5) = " << add_values(5, 5) << '\n';
+    std::cout << "Resta(5-5) = " << subtract_values(5, 5) << '\n';
+    std::cout << "Multiplicacion(5*5) = " << multiply_values(5, 5) << '\n';
+    std::cout << "Division(5/5) = " << divide_values(5, 5) << '\n';
     std::cout << "Factorial(5) = " << factorial(5) << '\n';
     std::vector<std::string> permutaciones = crearPermutaciones("123");
     std::cout << "Permutaciones de 123:\n";
--- tests/test1.cpp
+++ tests/test1.cpp
@@ -3,8 +3,8 @@
 
 TEST(MathTestCase, MathTestBasics)
 {
-	ASSERT_EQ(4, Suma(2, 2));
-	ASSERT_EQ(0, Resta(2, 2));
-	ASSERT_EQ(4, Multiplicacion(2, 2));
-	ASSERT_EQ(1, Division(2, 2));
+	ASSERT_EQ(4, add_values(2, 2));
+	ASSERT_EQ(0, subtract_values(2, 2));
+	ASSERT_EQ(4, multiply_values(2, 2));
+	ASSERT_EQ(1, divide_values(2, 2));
 }
