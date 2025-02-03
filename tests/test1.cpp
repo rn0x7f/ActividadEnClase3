2d120c7f87108ac5e58c5e844621c99653009ca1
rn0x7f <ampheriz0@gmail.com>
2025-02-02 18:10:29 -0600

    .

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,10 +1,13 @@
+
+// math_tests2.cpp
+// This file contains unit tests for MyMathLib using Google Test.
 #include <gtest/gtest.h>
 #include "BasicMath.h"
 
 TEST(MathTestCase, MathTestBasics)
 {
-	ASSERT_EQ(4, add_values(2, 2));
-	ASSERT_EQ(0, subtract_values(2, 2));
-	ASSERT_EQ(4, multiply_values(2, 2));
-	ASSERT_EQ(1, divide_values(2, 2));
+    ASSERT_EQ(4, add_values(2, 2));
+    ASSERT_EQ(0, subtract_values(2, 2));
+    ASSERT_EQ(4, multiply_values(2, 2));
+    ASSERT_EQ(1, divide_values(2, 2));
 }
--- tests/test2.cpp
+++ tests/test2.cpp
@@ -1,15 +1,18 @@
+
+// math_tests2.cpp
+// This file contains unit tests for MyMathLib using Google Test.
 #include <gtest/gtest.h>
 #include "ExtraMath.h"
 
 TEST(MathTestCase, MathTestExtra)
 {
-	ASSERT_EQ(120, factorial(5));
-	std::vector<std::string> permutaciones = crear_permutaciones("abc");
-	ASSERT_EQ(6, permutaciones.size());
-	ASSERT_EQ("abc", permutaciones[0]);
-	ASSERT_EQ("acb", permutaciones[1]);
-	ASSERT_EQ("bac", permutaciones[2]);
-	ASSERT_EQ("bca", permutaciones[3]);
-	ASSERT_EQ("cab", permutaciones[4]);
-	ASSERT_EQ("cba", permutaciones[5]);
+    ASSERT_EQ(120, factorial(5));
+    std::vector<std::string> permutaciones = crear_permutaciones("abc");
+    ASSERT_EQ(6, permutaciones.size());
+    ASSERT_EQ("abc", permutaciones[0]);
+    ASSERT_EQ("acb", permutaciones[1]);
+    ASSERT_EQ("bac", permutaciones[2]);
+    ASSERT_EQ("bca", permutaciones[3]);
+    ASSERT_EQ("cab", permutaciones[4]);
+    ASSERT_EQ("cba", permutaciones[5]);
 }
